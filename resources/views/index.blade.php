cc70e57afce6bf11296b606bece8e6b2b501ccda
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-22 19:05:13 -0300

    Adding responsiveness to the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container mx-auto px-4">
         <h2 class="text-blue-400 uppercase tracking-wide font-semibold">Popular Games</h2>
 
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -139,8 +139,8 @@
             </div>
         </div>
 
-        <div class="flex my-10">
-            <div class="recent-reviews w-3/4 mr-32">
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recent-reviews w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
 
                 <div class="recently-reviewed-container space-y-12 mt-8">
@@ -162,7 +162,7 @@
                                 Final Fantasy 7 Remake
                             </a>
                             <div class="text-gray-400 mt-1">PlayStation 4</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, soluta, ullam. A ab cum dicta libero minus reprehenderit sapiente soluta vero! A adipisci aliquam assumenda doloribus ea eaque eius enim et eveniet facere facilis iste laudantium, molestias numquam officia quaerat quibusdam quisquam reiciendis repellat repellendus similique suscipit tempore vel vero.
                             </p>
                         </div>
@@ -186,7 +186,7 @@
                                 Final Fantasy 7 Remake
                             </a>
                             <div class="text-gray-400 mt-1">PlayStation 4</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, soluta, ullam. A ab cum dicta libero minus reprehenderit sapiente soluta vero! A adipisci aliquam assumenda doloribus ea eaque eius enim et eveniet facere facilis iste laudantium, molestias numquam officia quaerat quibusdam quisquam reiciendis repellat repellendus similique suscipit tempore vel vero.
                             </p>
                         </div>
@@ -210,7 +210,7 @@
                                 Final Fantasy 7 Remake
                             </a>
                             <div class="text-gray-400 mt-1">PlayStation 4</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, soluta, ullam. A ab cum dicta libero minus reprehenderit sapiente soluta vero! A adipisci aliquam assumenda doloribus ea eaque eius enim et eveniet facere facilis iste laudantium, molestias numquam officia quaerat quibusdam quisquam reiciendis repellat repellendus similique suscipit tempore vel vero.
                             </p>
                         </div>
@@ -218,7 +218,7 @@
                 </div>
             </div>
 
-            <div class="most-anticipated w-1/4">
+            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
 
                 <div class="most-anticipated-container space-y-10 mt-8">
