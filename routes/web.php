c4e3109728b294746229702e96b533d999e0e060
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-22 19:34:57 -0300

    Styling the single game page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', fn() => view('index'));
+Route::get('/show', fn() => view('show'));
