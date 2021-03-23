9408830099392160245d3dcd73beb2f8cb54c44e
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-03-23 14:06:22 -0300

    Adding skeleton loading

--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -29,12 +29,17 @@
             </div>
         </div>
     @empty
-        <div class="flex mt-4">
-            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-            Loading
-        </div>
+        @foreach(range(1, 12) as $game)
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <div class="bg-gray-800 w-44 h-56"></div>
+                </div>
+
+                <div class="block text-transparent text-lg bg-gray-700 rounded leading-tight mt-4">
+                    title goes here
+                </div>
+                <div class="text-transparent bg-gray-700 inline-block rounded mt-3">PS4, PC, Switch</div>
+            </div>
+        @endforeach
     @endforelse
 </div>
--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -37,13 +37,25 @@
                 </div>
             </div>
         @empty
-            <div class="flex mt-4">
-                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                Loading
-            </div>
+            @foreach(range(1, 3) as $game)
+                <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                    <div class="relative flex-none">
+                        <div class="bg-gray-700 w-32 lg:w-48 h-40 lg:h-56"></div>
+                    </div>
+
+                    <div class="ml-12">
+                        <div class="inline-block text-lg font-semibold leading-tight text-transparent bg-gray-700 rounded mt-4">
+                            Title goes here
+                        </div>
+                        <div class="mt-8 space-y-2 hidden lg:block">
+                            <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum.</span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum.</span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum.</span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum.</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         @endforelse
     </div>
 </div>
