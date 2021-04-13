f163be9cca59efb039682ccf7a503bbaadc0347e
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-04-13 19:58:07 -0300

    Adding title to popular games component to keep consistency

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,15 @@
 
 @section('content')
     <div class="container mx-auto px-4">
-        <h2 class="text-blue-400 uppercase tracking-wide font-semibold">Popular Games</h2>
-
-        <livewire:popular-games>
+        <livewire:popular-games />
 
         <div class="flex flex-col lg:flex-row my-10">
-            <livewire:recently-reviewed>
+            <livewire:recently-reviewed />
 
             <div class="lg:w-1/4 mt-12 lg:mt-0">
-                <livewire:most-anticipated>
+                <livewire:most-anticipated />
 
-                <livewire:coming-soon>
+                <livewire:coming-soon />
             </div>
         </div>
     </div>
--- resources/views/livewire/popular-games.blade.php
+++ resources/views/livewire/popular-games.blade.php
@@ -1,9 +1,13 @@
-<div wire:init="fetch" class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
-    @forelse($games as $game)
-        <x-game-card :game="$game" :prefix="$prefix" />
-    @empty
-        @foreach(range(1, 12) as $game)
-            <x-game-card-skeleton />
-        @endforeach
-    @endforelse
+<div wire:init="fetch" class="popular-games border-b border-gray-800 pb-16">
+    <h2 class="text-blue-400 uppercase tracking-wide font-semibold">Popular Games</h2>
+
+    <div class="popular-games-container text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12">
+        @forelse($games as $game)
+            <x-game-card :game="$game" :prefix="$prefix" />
+        @empty
+            @foreach(range(1, 12) as $game)
+                <x-game-card-skeleton />
+            @endforeach
+        @endforelse
+    </div>
 </div>
