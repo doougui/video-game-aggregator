90d54e5a7c475c4dcc4e10a268c079c905f81c3f
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-04-21 14:03:09 -0300

    Adding Twitch and Discord environment variables to .env.example

--- config/services.php
+++ config/services.php
@@ -40,4 +40,10 @@ return [
         'client_secret' => env('DISCORD_CLIENT_SECRET'),
         'redirect' => env('DISCORD_REDIRECT_URI'),
     ],
+
+    'twitch' => [
+        'client_id' => env('TWITCH_CLIENT_ID'),
+        'client_secret' => env('TWITCH_CLIENT_SECRET'),
+        'redirect' => env('TWITCH_REDIRECT_URI'),
+    ],
 ];
