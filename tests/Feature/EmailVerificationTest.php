33a1066611d9826c47d64d030a5d75c370d61616
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-04-27 13:28:42 -0300

    Asserting that the verification email can be resent to an unverified user

--- tests/Feature/EmailVerificationTest.php
+++ tests/Feature/EmailVerificationTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\URL;
 use Tests\TestCase;
 
@@ -15,13 +17,15 @@ class EmailVerificationTest extends TestCase
 {
     use RefreshDatabase;
 
+    /** @test */
     protected function setUp(): void
     {
         parent::setUp();
         $this->refreshApplicationWithLocale(App::currentLocale());
     }
 
-    public function test_email_verification_alert_can_be_rendered()
+    /** @test */
+    public function email_verification_alert_can_be_rendered()
     {
         $user = User::factory()->create([
             'email_verified_at' => null,
@@ -33,7 +37,44 @@ class EmailVerificationTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_email_can_be_verified()
+    /** @test */
+    public function verification_email_is_sent_after_signing_up()
+    {
+        Notification::fake();
+
+        Notification::assertNothingSent();
+
+        $this->post(route('register'), [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'agreement' => true,
+        ]);
+
+        $this->assertAuthenticated();
+        Notification::assertSentTo(request()->user(), VerifyEmail::class);
+    }
+
+    /** @test */
+    public function verification_email_can_be_resent()
+    {
+        Notification::fake();
+
+        Notification::assertNothingSent();
+
+        $user = User::factory()->create([
+            'email_verified_at' => null,
+        ]);
+
+        $this->actingAs($user)->get(route('games.index'));
+        $this->post(route('verification.send'));
+
+        Notification::assertSentTo($user, VerifyEmail::class);
+    }
+
+    /** @test */
+    public function email_can_be_verified()
     {
         Event::fake();
 
@@ -54,7 +95,8 @@ class EmailVerificationTest extends TestCase
         $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
     }
 
-    public function test_email_is_not_verified_with_invalid_hash()
+    /** @test */
+    public function email_is_not_verified_with_invalid_hash()
     {
         $user = User::factory()->create([
             'email_verified_at' => null,
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,13 +2,8 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
@@ -42,24 +37,5 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(route('nickname'));
-    }
-
-    /** @test */
-    public function verification_email_is_sent_after_signing_up()
-    {
-        Notification::fake();
-
-        Notification::assertNothingSent();
-
-        $this->post(route('register'), [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
-            'agreement' => true,
-        ]);
-
-        $this->assertAuthenticated();
-        Notification::assertSentTo(request()->user(), VerifyEmail::class);
     }
 }
