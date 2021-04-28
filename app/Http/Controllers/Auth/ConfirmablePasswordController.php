0db46a4d0a082d1fd839e9ba0e393c9c1874821e
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-04-28 13:56:56 -0300

    Skipping password confirmation if user is using social login

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -13,29 +13,37 @@ class ConfirmablePasswordController extends Controller
     /**
      * Show the confirm password view.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\View\View
+     * @param Request $request
      */
     public function show(Request $request)
     {
+        if (auth()->user()->isAuthenticatedWithSocialLogin()) {
+            $request->session()->put('auth.password_confirmed_at', time());
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
         return view('auth.confirm-password');
     }
 
     /**
      * Confirm the user's password.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return mixed
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
-        if (! Auth::guard('web')->validate([
-            'email' => $request->user()->email,
-            'password' => $request->password,
-        ])) {
-            throw ValidationException::withMessages([
-                'password' => __('auth.password'),
-            ]);
+        if (! auth()->user()->isAuthenticatedWithSocialLogin()) {
+            if (!Auth::guard('web')->validate([
+                'email' => $request->user()->email,
+                'password' => $request->password,
+            ])) {
+                throw ValidationException::withMessages([
+                    'password' => __('auth.password'),
+                ]);
+            }
         }
 
         $request->session()->put('auth.password_confirmed_at', time());
