a409031f0824f8417baa66f5f8de5c4f397c8ec3
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-04-29 13:46:27 -0300

    Setting defaultStringLength to 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 }
